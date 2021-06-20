5e63d3ff9ca2f3523d21f7a5e4d871d773e2654d
KQué <77649679+KQue6@users.noreply.github.com>
2021-06-20 10:33:23 -0700

    creating User Buikd Resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "Candy", password: "123456789")
+User.create(username: "Soda", password: "123456789")
+User.create(username: "Chicken", password: "123456789")
+User.create(username: "Noodles", password: "123456789")
+User.create(username: "Chair", password: "123456789")
+User.create(username: "NameSpace", password: "123456789")
