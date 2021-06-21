8f9ec85dbdbf122a94e41ac7fa14e44605675ed2
KQué <77649679+KQue6@users.noreply.github.com>
2021-06-21 16:29:08 -0700

    Created Authentication System(Create/Destroy) Sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,24 @@
 class SessionsController < ApplicationController
 
-  def new 
+  def new
+  end
 
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Quext Credentials Accepted"
+      redirect_to root_path
+    else
+      falsh.now[:error] = "Login Credentials Not Corrrect"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You Have Logged Out Of Quext"
+    redirect_to login_path
   end
 
 end
