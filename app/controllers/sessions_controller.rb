6dfa311f4472bf7f4d25e97ac4cf56de4a220de0
KQué <77649679+KQue6@users.noreply.github.com>
2021-06-21 17:28:22 -0700

    created View Restrictions For Logged In Users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
+
 
   def new
   end
@@ -19,6 +21,14 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:success] = "You Have Logged Out Of Quext"
     redirect_to login_path
+  end
+
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You Are Currently Signed In"
+      redirect_to root_path
+    end
   end
 
 end
