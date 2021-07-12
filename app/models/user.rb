e894953bcdfacded5ce518f87170ac686622307f
KQué <77649679+KQue6@users.noreply.github.com>
2021-07-11 21:52:50 -0700

    Finally,  I think I Changed The right Line Of Code To Fix,Whatever Caused The What Was Wrong.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: { minimum: 3, maximum: 18 },
-            uniqueness: {case_sensitive: false }
+  validates :username, presence: true, length: { minimum: 4, maximum: 36 },
+            uniqueness: { case_sensitive: false }
   has_many :messages
   has_secure_password
 end
